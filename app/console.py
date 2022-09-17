350e0761ddd9da036c92905927269eafc72306c7
Ethan Cloin <psnethan@gmail.com>
2022-09-16 22:00:24 -0400

    works when run as module

---
+++ app/console.py
@@ -0,0 +1,6 @@
+"""This must be run as a module not a script to find the dependencies. AKA run with python3 -m app.console"""
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("app.api.v1.endpoints.plant:app", host="0.0.0.0", port=8000)
